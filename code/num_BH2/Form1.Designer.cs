1961d519613aced1c7cba16ef26e5c95b490719c
urkax <1751772593@qq.com>
2020-01-17 14:36:11 +0800

    x

---
+++ code/num_BH2/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace num_BH2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_x = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_result = new System.Windows.Forms.TextBox();
+            this.button_solve = new System.Windows.Forms.Button();
+            this.comboBox_method = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel_Taylor = new System.Windows.Forms.Panel();
+            this.numericUpDown_Taylor = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel_ODE = new System.Windows.Forms.Panel();
+            this.numericUpDown_ODE = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox_error = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel_Taylor.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_Taylor)).BeginInit();
+            this.panel_ODE.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_ODE)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox_x
+            // 
+            this.textBox_x.Location = new System.Drawing.Point(112, 79);
+            this.textBox_x.Name = "textBox_x";
+            this.textBox_x.Size = new System.Drawing.Size(100, 25);
+            this.textBox_x.TabIndex = 0;
+            this.textBox_x.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_x_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "输入x：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 257);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "结果：";
+            // 
+            // textBox_result
+            // 
+            this.textBox_result.Location = new System.Drawing.Point(83, 257);
+            this.textBox_result.Multiline = true;
+            this.textBox_result.Name = "textBox_result";
+            this.textBox_result.ReadOnly = true;
+            this.textBox_result.Size = new System.Drawing.Size(226, 79);
+            this.textBox_result.TabIndex = 6;
+            // 
+            // button_solve
+            // 
+            this.button_solve.Location = new System.Drawing.Point(303, 192);
+            this.button_solve.Name = "button_solve";
+            this.button_solve.Size = new System.Drawing.Size(75, 47);
+            this.button_solve.TabIndex = 7;
+            this.button_solve.Text = "求解";
+            this.button_solve.UseVisualStyleBackColor = true;
+            this.button_solve.Click += new System.EventHandler(this.button_solve_Click);
+            // 
+            // comboBox_method
+            // 
+            this.comboBox_method.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_method.FormattingEnabled = true;
+            this.comboBox_method.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.comboBox_method.Items.AddRange(new object[] {
+            "逼近法",
+            "常微分方程",
+            "cordic算法"});
+            this.comboBox_method.Location = new System.Drawing.Point(112, 134);
+            this.comboBox_method.Name = "comboBox_method";
+            this.comboBox_method.Size = new System.Drawing.Size(121, 23);
+            this.comboBox_method.TabIndex = 8;
+            this.comboBox_method.SelectedIndexChanged += new System.EventHandler(this.comboBox_method_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 142);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "计算方法：";
+            // 
+            // panel_Taylor
+            // 
+            this.panel_Taylor.Controls.Add(this.numericUpDown_Taylor);
+            this.panel_Taylor.Controls.Add(this.label6);
+            this.panel_Taylor.Location = new System.Drawing.Point(30, 174);
+            this.panel_Taylor.Name = "panel_Taylor";
+            this.panel_Taylor.Size = new System.Drawing.Size(195, 55);
+            this.panel_Taylor.TabIndex = 10;
+            this.panel_Taylor.Visible = false;
+            // 
+            // numericUpDown_Taylor
+            // 
+            this.numericUpDown_Taylor.Location = new System.Drawing.Point(68, 12);
+            this.numericUpDown_Taylor.Maximum = new decimal(new int[] {
+            150,
+            0,
+            0,
+            0});
+            this.numericUpDown_Taylor.Minimum = new decimal(new int[] {
+            11,
+            0,
+            0,
+            0});
+            this.numericUpDown_Taylor.Name = "numericUpDown_Taylor";
+            this.numericUpDown_Taylor.Size = new System.Drawing.Size(65, 25);
+            this.numericUpDown_Taylor.TabIndex = 4;
+            this.numericUpDown_Taylor.Value = new decimal(new int[] {
+            11,
+            0,
+            0,
+            0});
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 17);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 15);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "项数k=";
+            // 
+            // panel_ODE
+            // 
+            this.panel_ODE.Controls.Add(this.label8);
+            this.panel_ODE.Controls.Add(this.numericUpDown_ODE);
+            this.panel_ODE.Controls.Add(this.label7);
+            this.panel_ODE.Location = new System.Drawing.Point(29, 172);
+            this.panel_ODE.Name = "panel_ODE";
+            this.panel_ODE.Size = new System.Drawing.Size(210, 55);
+            this.panel_ODE.TabIndex = 11;
+            this.panel_ODE.Visible = false;
+            // 
+            // numericUpDown_ODE
+            // 
+            this.numericUpDown_ODE.Location = new System.Drawing.Point(88, 15);
+            this.numericUpDown_ODE.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown_ODE.Minimum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.numericUpDown_ODE.Name = "numericUpDown_ODE";
+            this.numericUpDown_ODE.Size = new System.Drawing.Size(65, 25);
+            this.numericUpDown_ODE.TabIndex = 4;
+            this.numericUpDown_ODE.Value = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(3, 17);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(83, 15);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "划分次数t=";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(159, 18);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(47, 15);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "*10^3";
+            // 
+            // textBox_error
+            // 
+            this.textBox_error.Location = new System.Drawing.Point(82, 350);
+            this.textBox_error.Multiline = true;
+            this.textBox_error.Name = "textBox_error";
+            this.textBox_error.ReadOnly = true;
+            this.textBox_error.Size = new System.Drawing.Size(227, 79);
+            this.textBox_error.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 350);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "误差：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(24, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 24);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "求解sin(x)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 453);
+            this.Controls.Add(this.panel_ODE);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_error);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel_Taylor);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox_method);
+            this.Controls.Add(this.button_solve);
+            this.Controls.Add(this.textBox_result);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_x);
+            this.Name = "Form1";
+            this.Text = "sin(x)";
+            this.panel_Taylor.ResumeLayout(false);
+            this.panel_Taylor.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_Taylor)).EndInit();
+            this.panel_ODE.ResumeLayout(false);
+            this.panel_ODE.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_ODE)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_x;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_result;
+        private System.Windows.Forms.Button button_solve;
+        private System.Windows.Forms.ComboBox comboBox_method;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel_Taylor;
+        private System.Windows.Forms.NumericUpDown numericUpDown_Taylor;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel_ODE;
+        private System.Windows.Forms.NumericUpDown numericUpDown_ODE;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox_error;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ code/num_BH2/Form1.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace num_BH2
+{
+    public partial class Form1 : Form
+    {
+        int error_m = 15;
+        double[] tangent;
+        double[] angle;
+        double K;
+        int cordic_n=15;
+
+    public Form1()
+        {
+            InitializeComponent();
+
+            //double error = Taylor_error(11, 15, 2 * Math.PI);
+            //double result = Taylor_calculate(13, 15, Math.PI/2);
+            //ODE_error(Math.PI / 4 / 12000);
+            //ODE_calculate(Math.PI / 4, 12000);
+
+            cordic_init();
+        }
+
+        //x只能输入数字
+        private void textBox_x_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 0x20) e.KeyChar = (char)0;  //禁止空格键  
+            if ((e.KeyChar == 0x2D) && (((TextBox)sender).Text.Length == 0)) return;   //处理负数  
+            if (e.KeyChar > 0x20)
+            {
+                try
+                {
+                    double.Parse(((TextBox)sender).Text + e.KeyChar.ToString());
+                }
+                catch
+                {
+                    e.KeyChar = (char)0;   //处理非法字符  
+                }
+            }
+        }
+
+        //逼近法计算
+        public double Taylor_calculate(int k, double x0)
+        {
+            double result = 0;
+            for(int i=1;i<=k;i++)
+            {
+                double f_i = 1;
+                for(int j=1;j<=2*i-1;j++)
+                {
+                    f_i *= (x0 / j);
+                }
+
+                if(i%2==0)
+                {
+                    result -= f_i;
+                }
+                else
+                {
+                    result += f_i;
+                }
+            }
+
+            return result;
+        }
+
+        //逼近法误差计算
+        public double Taylor_error(int k, int m, double x0)
+        {
+            double error1 = 1;
+            for(int i=1;i<=2*k+1;i++)
+            {
+                error1 *= (x0 / i);
+            }
+
+            double ten_m = 0.5;
+            for(int i=0;i<m;i++)
+            {
+                ten_m *= 0.1;
+            }
+            double error2 = ten_m * (k - 1);
+
+            double error3 = 0;
+            for(int i=1;i<=k;i++)
+            {
+                double error3_i = 1;
+                for(int j=1;j<=2*i-2;j++)
+                {
+                    error3_i *= (x0 / j);
+                }
+                error3 += error3_i;
+            }
+
+            error3 *= ten_m;
+
+            return error1 + error2 + error3;
+        }
+
+        //常微分方程法计算
+        public double ODE_calculate(double x, int t)
+        {
+            double h = x / t;
+            double y_i = 0;
+            for(int i=0;i<t;i++)
+            {
+                double f_i = my_sqrt(1 - y_i * y_i, 0.5, 200);
+                y_i = y_i + h * f_i;
+            }
+
+            return y_i;
+        }
+
+        //常微分方程法误差计算
+        public double ODE_error(double h)
+        {
+            h = h >= 0 ? h : -h;
+            if(h==0)
+            {
+                return 0;
+            }
+
+            double M = 1.2688;
+            double pow_h = Math.Pow(1 + M * h, Math.PI / 4 / h);
+            double error1 = 2 * pow_h * Math.Pow(10, -15)/M/h;
+            double error2 = (pow_h - 1) * h / M;
+            double result = error1 + error2;
+            return result;
+        }
+
+        //cordic算法初始化，准备参数
+        public void cordic_init()
+        {
+            tangent = new double[cordic_n];
+            for (int i = 0; i < cordic_n; i++)
+            {
+                tangent[i] = 1;
+                for(int j=0;j<i;j++)
+                {
+                    tangent[i] /= 2.0;
+                }
+            }
+
+            angle = new double[cordic_n];
+            for (int i = 0; i < cordic_n; i++)
+            {
+                angle[i] = Math.Atan(tangent[i]);
+            }
+
+            K = 1;
+            for (int j = 0; j < cordic_n; j++)
+            {
+                K *= 1 / Math.Sqrt(1 + (tangent[j]) * (tangent[j]));
+            }
+        }
+
+        //cordic算法计算
+        public void cordic_calculate(double x, out double sin_y, out double cos_x)
+        {
+            int i, sign;
+            double cos_last, sin_last, angle_delta;
+            cos_x = 0; sin_y = 0; angle_delta = x;
+            cos_last = 1; sin_last = 0;
+            sign = 1;
+
+            for (i = 0; i < cordic_n; i++)
+            {
+                cos_x = cos_last - sign * sin_last * tangent[i];
+                sin_y = sin_last + sign * cos_last * tangent[i];
+                angle_delta = angle_delta - sign * angle[i];
+                
+                cos_last = cos_x;
+                sin_last = sin_y;
+
+                sign = angle_delta > 0 ? 1 : -1;
+            }
+
+            sin_y *= K;
+            cos_x *= K;
+        }
+
+        //方程求根的数值解，求解根号
+        public double my_sqrt(double q, double x0, int n)
+        {
+            double result=x0;
+            for(int i=0;i<n;i++)
+            {
+                result = result - result * result / 4 + q / 4;
+            }
+
+            return result;
+        }
+
+        private void button_solve_Click(object sender, EventArgs e)
+        {
+            textBox_result.Text = "";
+            textBox_error.Text = "";
+            if(textBox_x.Text=="")
+            {
+                MessageBox.Show(this, "请先输入x！", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            double x = double.Parse(textBox_x.Text);
+            if (Math.Abs(x / 2 / Math.PI) >= 1)
+            {
+                x = x - Math.PI * 2 * ((int)(x / 2 / Math.PI));//x归一化为0到2pi
+            }
+            
+            string method = comboBox_method.Text;
+            if (method == "逼近法")
+            {
+                int k = (int)numericUpDown_Taylor.Value;
+                double result = Taylor_calculate(k, x);
+                double error = Taylor_error(k, error_m, x);
+
+                textBox_result.Text = result.ToString();
+                textBox_error.Text = error.ToString();
+            }
+            else if (method == "常微分方程")
+            {
+                double sign = 1;
+                if(x>=Math.PI*3/2)
+                {
+                    x = x - 2 * Math.PI;
+                }
+                else if(x>=Math.PI/2)
+                {
+                    x = x - Math.PI;
+                    sign = -1;
+                }
+
+                int t = (int)numericUpDown_ODE.Value*1000;
+                double sin_x0 = ODE_calculate(x / 2, t);
+                double result = sign * 2 * sin_x0 * my_sqrt(1 - sin_x0 * sin_x0, 0.5, 200);
+                double error = ODE_error(x / 2 / t);
+
+                textBox_result.Text = result.ToString();
+                textBox_error.Text = error.ToString();
+            }
+            else if(method == "cordic算法")
+            {
+                double sign = 1;
+                bool cos_flag = false;
+                if (x >= Math.PI)
+                {
+                    x = x - Math.PI;
+                    sign = -1;
+                }
+                if (x >= Math.PI / 2)
+                {
+                    x = x - Math.PI/2; //cos(x-pi/2)=sin(x)
+                    cos_flag = true;
+                }
+
+                cordic_calculate(x, out double sin_y, out double cos_x);
+                double result = sign*(cos_flag ? cos_x : sin_y);
+                textBox_result.Text = result.ToString();
+            }
+            else
+            {
+                MessageBox.Show(this, "请先选择计算方法！", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+        }
+
+        private void comboBox_method_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string method = comboBox_method.Text;
+            panel_ODE.Visible = false;
+            panel_Taylor.Visible = false;
+
+            //逼近法 常微分方程 cordic算法
+            if (method=="逼近法")
+            {
+                panel_Taylor.Visible = true;
+            }
+            else if(method=="常微分方程")
+            {
+                panel_ODE.Visible = true;
+            }
+        }
+    }
+}
